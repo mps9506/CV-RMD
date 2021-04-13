cb46dbb42540559fe40478036fea18947c0a4e78
Michael Schramm <mpschramm@gmail.com>
2021-04-13 10:47:59 -0500

    April update

--- CVR/schramm-cv-long.R
+++ CVR/schramm-cv-long.R
@@ -1,29 +1,50 @@
-library(CVR)
-library(RefManageR)
+library(mpsCVR)
+#library(RefManageR)
 
-######
 
-key <- Sys.getenv("zotero_key")
+#########
+# Notes #
+#########
 
-peer_review <- RefManageR::ReadZotero(group = "2533336",
-                                      .params = list(key = key,
-                                                   itemType = "journalArticle"))
+# Does not work with latest pandoc for unknown reason.
+# roll back to pandoc ~2.11
 
-tech_report <- ReadZotero(group = "2533336",
-                          .params=list(key = key,
-                                       itemType = "report"))
+# Use better bibtex zotero plugin for exporting .bib from Zotero db
+# previously used RefManageR but had issues with fields properly exporting
+# and using the notes field to export custom fields. Would be great to
+# programatically export the bib files from the local
+# Zotero install using the bbt plugin.
 
-conference <- ReadZotero(group = "2533336",
-                         .params=list(key = key,
-                                      itemType = "presentation"))
 
-software <- ReadZotero(group = "2533336",
-                       .params=list(key = key,
-                                    itemType = "computerProgram"))
+# 
+# key <- Sys.getenv("zotero_key")
+# 
+# peer_review <- RefManageR::ReadZotero(group = "2533336",
+#                                       .params = list(key = key,
+#                                                    itemType = "journalArticle"))
+# 
+# tech_report <- ReadZotero(group = "2533336",
+#                           .params=list(key = key,
+#                                        itemType = "report"))
+# ## To read these in you must set check.entries to FALSE or "warn"
+# old.opts <- BibOptions(check.entries = FALSE)
+# conference <- ReadZotero(group = "2533336",
+#                          .params=list(key = key,
+#                                       tag = "presentation"))
+# 
+# software <- ReadZotero(group = "2533336",
+#                        .params=list(key = key,
+#                                     itemType = "computerProgram"))
+# 
+# datasets <- ReadZotero(group = "2533336",
+#                        .params=list(key = key,
+#                                     tag = "dataset"))
 
-datasets <- ReadZotero(group = "2533336",
-                       .params=list(key = key,
-                                    tag = "dataset"))
+peer_review <- fs::path_wd("CVR/peer_review.bib")
+tech_report <- fs::path_wd("CVR/tech_report.bib")
+conference <- fs::path_wd("CVR/conference.bib")
+software <- fs::path_wd("CVR/software.bib")
+datasets <- fs::path_wd("CVR/datasets.bib")
 
 build_cv(full_name = "Michael Schramm",
          data_location = "https://docs.google.com/spreadsheets/d/1oeMexIuFWJIJa-xofMLqSvPtg49VceJqM2y3YDqcW-4/edit?usp=sharing",
@@ -33,5 +54,7 @@ build_cv(full_name = "Michael Schramm",
          software = software,
          datasets = datasets,
          output_file_name = "schramm-cv-long.pdf",
-         output_dir = here::here("/CVR"),
+         rmd_template = "svm_cv",
+         output_dir = here::here("CVR"),
          spell_check = FALSE)
+
